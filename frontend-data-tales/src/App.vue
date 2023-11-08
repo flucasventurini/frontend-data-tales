94d5482a9e30cdcbf6a4edc6cfd6d008a9eca78a
Venturini <b1400203@Hyundai-Brasil.com>
2023-11-07 21:33:12 -0300

    envio inicial

---
+++ frontend-data-tales/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+   <componentePaginaInicial/>
+</template>
+
+<script>
+  import componentePaginaInicial from './components/paginaInicial.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      componentePaginaInicial
+    }
+  }
+</script>
+
---
+++ frontend-data-tales/src/components/paginaInicial.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1 id="titulo">Data tales</h1>
+    <p>foi mal ter feito só isso kennedy, amanhã avanço mais kkkkkk <br>
+        (a estrutura ta feita)
+    </p>
+</template>
+
+<style>
+#titulo {
+    color: blueviolet;
+}
+</style>
+
+<script>
+    export default {
+        name: 'componentePaginaInicial'
+    }
+</script>
